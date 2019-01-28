b3e78dd7c9923acbed236541eaff9da57a0767f3
aakash sharma <akkysharma20@yahoo.in>
2019-01-29 00:52:43 +0530

    socketprogramming

---
+++ server_tcp.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <netinet/in.h>         
+#include <arpa/inet.h>
+
+#define PORT 4455
+#define SIZE 1024
+
+int main(int argc, char const *argv[])
+{	
+	int sockfd;
+	struct sockaddr_in serverAddr;
+
+	int newSocket;
+	struct sockaddr_in newAddr;
+
+	socklen_t addr_size;	
+	char buffer[SIZE];
+
+
+	sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serverAddr, '\0' , sizeof(serverAddr));
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	listen(sockfd, 5);
+	addr_size = sizeof(newAddr);
+	newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+	strcpy(buffer, "Hello");
+	send(newSocket, buffer , strlen(buffer) , 0);
+	return 0;
+}
